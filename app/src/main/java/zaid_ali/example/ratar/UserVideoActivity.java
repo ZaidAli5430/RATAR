eb0e8b57d50654c6622e6253fc0d768b15629bd3
zaid-ali <zaid.ali5430@gmail.com>
2021-03-20 14:57:40 +0500

    overide onResume in UserVideoActivity to check the required permissions

--- app/src/main/java/zaid_ali/example/ratar/UserVideoActivity.java
+++ app/src/main/java/zaid_ali/example/ratar/UserVideoActivity.java
@@ -1,13 +1,23 @@
 package zaid_ali.example.ratar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -17,6 +27,12 @@ public  class UserVideoActivity extends AppCompatActivity implements GLSurfaceVi
     private boolean isMuted = false;
     private ImageView muteBtn;
     private boolean isCalling = true;
+    private boolean installRequested;
+    private Session mSession;
+    private Snackbar mMessageSnackbar;
+    private GLSurfaceView mSurfaceView;
+
+//    private DisplayRotationHelper mDisplayRotationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +40,106 @@ public  class UserVideoActivity extends AppCompatActivity implements GLSurfaceVi
         setContentView(R.layout.activity_user_video);
 
         muteBtn = findViewById(R.id.btn_mute);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        if (mSession == null) {
+            Exception exception = null;
+            String message = null;
+            try {
+                switch (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+                    case INSTALL_REQUESTED:
+                        installRequested = true;
+                        return;
+                    case INSTALLED:
+                        break;
+                }
+
+                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+                // permission on Android M and above, now is a good time to ask the user for it.
+                if (!CameraPermissionHelper.hasCameraPermission(this)) {
+                    CameraPermissionHelper.requestCameraPermission(this);
+                    return;
+                }
+
+                mSession = new Session(/* context= */ this);
+            } catch (UnavailableArcoreNotInstalledException
+                    | UnavailableUserDeclinedInstallationException e) {
+                message = "Please install ARCore";
+                exception = e;
+            } catch (UnavailableApkTooOldException e) {
+                message = "Please update ARCore";
+                exception = e;
+            } catch (UnavailableSdkTooOldException e) {
+                message = "Please update this app";
+                exception = e;
+            } catch (Exception e) {
+                message = "This device does not support AR";
+                exception = e;
+            }
+
+            if (message != null) {
+                showSnackbarMessage(message, true);
+//                Log.e(TAG, "Exception creating session", exception);
+                return;
+            }
+
+            // Create default config and check if supported.
+            Config config = new Config(mSession);
+            if (!mSession.isSupported(config)) {
+                showSnackbarMessage("This device does not support AR", true);
+            }
+            mSession.configure(config);
+        }
+
+        showLoadingMessage();
+        // Note that order matters - see the note in onPause(), the reverse applies here.
+        mSession.resume();
+        mSurfaceView.onResume();
+//        mDisplayRotationHelper.onResume();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
     }
 
+    private void showSnackbarMessage(String message, boolean finishOnDismiss) {
+        mMessageSnackbar = Snackbar.make(
+                UserVideoActivity.this.findViewById(android.R.id.content),
+                message, Snackbar.LENGTH_INDEFINITE);
+        mMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+        if (finishOnDismiss) {
+            mMessageSnackbar.setAction(
+                    "Dismiss",
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mMessageSnackbar.dismiss();
+                        }
+                    });
+            mMessageSnackbar.addCallback(
+                    new BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                        @Override
+                        public void onDismissed(Snackbar transientBottomBar, int event) {
+                            super.onDismissed(transientBottomBar, event);
+                            finish();
+                        }
+                    });
+        }
+        mMessageSnackbar.show();
+    }
+
+    private void showLoadingMessage() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                showSnackbarMessage("Searching for surfaces...", false);
+            }
+        });
+    }
 
     public void onCallClicked(View view) {
         if (isCalling) {
@@ -52,10 +164,6 @@ public  class UserVideoActivity extends AppCompatActivity implements GLSurfaceVi
         muteBtn.setImageResource(isMuted ? R.drawable.mute_button_icon : R.drawable.unmute_button_icon);
     }
 
-    @Override
-    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-
-    }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int i, int i1) {
